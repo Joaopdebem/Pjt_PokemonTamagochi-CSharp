77584f51465d9ac2c058111e801aa8d0df552d72
JoÃ£o Pedro de Bem <jpdebem@gmail.com>
2023-09-19 12:11:41 -0300

    Commit inicial

---
+++ Pokemagochi/Pokemagochi/Funcionamento/FuncoesPokeAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemagochi.Funcionamento
+{
+    public class FuncoesPokeAPI
+    {
+        public int count { get; set; }
+        public dynamic next { get; set; }
+        public dynamic previous { get; set; }
+
+    }
+}
---
+++ Pokemagochi/Pokemagochi/Pokemons/Mascote.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemagochi.Pokemons
+{
+    public class Mascote
+    {
+        public List<Abilities> Habilidades { get; set; }
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+        public string Nome { get; set; }
+        public int Alimentacao { get; set; }
+        public int Humor { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public Mascote()
+        {
+            Random valorRandomic = new Random();
+            Alimentacao = valorRandomic.Next(2, 10);
+            Humor = valorRandomic.Next(2, 10);
+            DataNascimento = DateTime.Now;
+        }
+
+        public bool VerificarFome()
+        {
+            return this.Alimentacao < 5;
+        }
+        public void AlimentarMascote()
+        {
+            this.Alimentacao++;
+        }
+        public void BrincarMascote()
+        {
+            this.Humor++;
+            this.Alimentacao--;
+        }
+        public bool SaudeMascote()
+        {
+            return (this.Alimentacao > 0 && this.Humor > 0);
+        }
+
+    }
+}
